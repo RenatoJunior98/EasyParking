8dfccd276d9abec9cb5039e6fb1553593e3be6f6
Ricardo Cruz <2000ytricardo@gmail.com>
2020-12-09 18:23:21 +0000

    Criação de ficheiro js

---
+++ code/public/javascripts/parquesLista.js
@@ -0,0 +1,68 @@
+var studentName = "John Smith";
+
+// [nome do parque, lugares disponíveis, preco diário]
+var parque1 = ["Parque do Marquês de Pombal", 57, 10]; 
+var parque2 = ["Parque do Hospital da Luz", 0, 15]; 
+
+var parques = [parque1, parque2];
+
+function validation(object) {
+    let aux = '';
+    if (object[1] == 0){
+        aux = 'class = "full"';
+    }
+    return aux;
+}
+
+function unit(n){ // returns an object's array from the array of parques.
+    for (let i = 0; i < units.length; i++){
+        aux = parques[i];
+        if (n == i){
+            return aux;
+        }
+    }
+}
+
+function unitSlot(object) { // returns all "parques" squares with their information for main tag.
+    sum = "";
+    for (let i = 0; i < object.length; i++){
+    aux = unit(i);
+    icon = aux[0].substring(0, 2);
+    slot =
+    '<section '+validation(aux)+'class = "parqueSections">'+'<section id="top-of-slot"><h1 id="icon">'+icon+'</h1><section id="precoDiario"><h1>'+aux[0]+'</h1>'+'<p>Grade: '+aux[1]+'</p></section></section><section id="bottom-of-slot"><p>Preço: '+aux[2]+'</p></section></section>';
+    sum = sum + slot;
+    }
+    return sum;
+    
+}
+
+window.onload =  function () {
+   
+    document.getElementById('student');
+    student.innerText = studentName + " grades";
+
+    document.getElementById('grades');
+    grades.innerHTML = unitSlot(units);
+
+
+    failed = 0;
+    livres = 0;
+    sum = 0;
+    for (let i = 0; i < units.length; i++){
+        aux = unit(i);
+        if (aux[1]< 9.5){
+            sum = sum;
+            full++;
+        } else {
+        sum = sum + aux[1];
+        livres++;
+        }
+    }
+
+    average = 0;
+    average = livres / passed;
+
+    document.getElementById('summary');
+    summary.innerHTML = '<summary><b>Average: '+average.toFixed(1)+'</summary><p>'+full+' failed units</p><p>'+livres+' passed units</p></b>';
+    
+}
