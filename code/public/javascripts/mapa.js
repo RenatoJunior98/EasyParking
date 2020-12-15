b64761931633dbd533e78a63d7ba04a2a9c0206e
ricardocruz00 <56094485+ricardocruz00@users.noreply.github.com>
2020-12-15 09:47:47 +0000

    mapa.js created

---
+++ code/public/javascripts/mapa.js
@@ -0,0 +1,32 @@
+
+
+
+var mymap = L.map('mapid').setView([38.70722, -9.15252], 15);
+var mapMark = L.icon({
+  iconUrl: 'orangemapmark.png',
+  iconSize: [38, 95], // size of the icon
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+var marker = L.marker([38.70722, -9.15254]).addTo(mymap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 21,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoiMjAwMHl0cmljYXJkbyIsImEiOiJja2llYzA2aDkweDJsMnRyc2NmYTdlb2hmIn0.wvxtVtYjr_2Us5p1V6lGyg'
+}).addTo(mymap);
+
+
+var searchControl = L.esri.Geocoding.geosearch().addTo(mymap);
+
+var results = L.layerGroup().addTo(mymap);
+
+searchControl.on('results', function (data) {
+  results.clearLayers();
+  //  for (var i = data.results.length - 1; i >= 0; i--) {
+  //    results.addLayer(L.marker(data.results[i].latlng));
+  // }
+});
